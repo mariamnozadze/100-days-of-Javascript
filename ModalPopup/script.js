d93bc8dc2ad47c3887b10d4cf0fc3907bff0cdbd
mariamnozadze <mariamnozadze97@yahoo.com>
2024-01-18 13:24:11 +0400

    - javaascript for modal

---
+++ ModalPopup/script.js
@@ -0,0 +1,19 @@
+// variables
+const modal = document.querySelector(".modal");
+const btn = document.querySelector(".btn");
+const close = document.querySelector(".close");
+
+btn.addEventListener("click", openModal);
+close.addEventListener("click", closeModal);
+modal.addEventListener("click", closeModal);
+
+// open modal
+function openModal(e) {
+  e.preventDefault();
+  modal.style.display = "block";
+}
+
+// close modal
+function closeModal(e) {
+  modal.style.display = "none";
+}
