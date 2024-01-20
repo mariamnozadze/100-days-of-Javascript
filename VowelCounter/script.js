6eff4aa74b84907c35b9fc48082fa9bab5106a43
mariamnozadze <mariamnozadze97@yahoo.com>
2024-01-20 12:14:54 +0400

    - starting files for vovel counter

---
+++ VowelCounter/script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".result");
+const inputText = document.querySelector(".input-text");
+
+btn.addEventListener("click", isPalindrome);
