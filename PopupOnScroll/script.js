9523a4f75d2a858aec9c2f1ce7276f0873a37714
mariamnozadze <mariamnozadze97@yahoo.com>
2024-02-13 12:14:52 +0400

    - scroll progress, sticky header, popup

---
+++ PopupOnScroll/script.js
@@ -0,0 +1,37 @@
+// Sticky Header
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// Scroll Indicator JS
+window.onscroll = () => scrollProgress();
+
+function scrollProgress() {
+  const currentState =
+    document.body.scrollTop || document.documentElement.scrollTop;
+
+  const pageHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+
+  const scrollPercentage = (currentState / pageHeight) * 100;
+
+  const progressBar = document.querySelector(".progress");
+
+  progressBar.style.visibility = "visible";
+  progressBar.style.width = scrollPercentage + "%";
+
+  // Popup
+  const newsLetter = document.querySelector(".newsletter");
+
+  if (scrollPercentage > 80) {
+    newsLetter.style.transform = "translateX(0)";
+  } else {
+    newsLetter.style.transform = "translateX(-100%)";
+  }
+
+  document.querySelector(".fa-times").addEventListener("click", () => {
+    newsLetter.style.transform = "translateX(-100%)";
+  });
+}
